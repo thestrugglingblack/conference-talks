355914364feab52a1c462b62bd5d0de13c151208
zuri.hunter <zuri.hunter@thehumangeo.com>
2017-04-12 19:48:12 -0400

    build out basics of repo

---
+++ APPLICATIONS/front-end-101/js/custom.js
@@ -0,0 +1,38 @@
+/*
+  BOOLEANS
+  ===================
+  !=        NOT
+  ===       EQUAL (Value and DataType)
+  ==        EQUAL
+  &&        AND
+  ||        OR
+
+  VARIABLE DECLARATION
+  ====================
+  var someVariable = "Welcome to Front-End 101"
+
+  DATATYPES
+  ====================
+  "some String"                         Strings
+  101                                   Integers
+  ["One", "Two", "Three"]               Arrays
+  {"propertyName": "Property Value"}    Object
+
+  OPERATORS
+  ====================
+  /         Division
+  +         Addition
+  -         Subtraction
+  *         Multipulcation
+  %         Modulus
+
+  CREATE FUNCTION
+  ====================
+  function nameOfFunction (ParametersA, ParametersB){
+    //BLOCK OF CODE
+    ParametersA + ParametersB
+  }
+
+  nameOfFunction(100 + 1)
+
+*/
